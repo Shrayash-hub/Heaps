5cbd3d32c2edb4ff61a3f7e27d299019a762a258
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-01-21 09:50:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (17.26%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+
+        vector<int> previous(n+1);
+        previous[0] = 1;
+        previous[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            previous[i] = previous[i-1] + previous[i-2];
+        }
+        return previous[n];
+    }
+};
