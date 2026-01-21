7b8f6369f6bb70b691ebe8155ec19d9d76da953e
Shrayash awasthi <awasthishrayashofc@gmail.com>
2026-01-21 09:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.70%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,9 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0) return 0;
-        if(n<2) return 1;
+        if(n == 0) return 0;
+        if(n == 1) return 1;
 
-        return fib(n-1) + fib(n-2);
+        vector<int> previous(n+1);
+        previous[0] = 0;
+        previous[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            previous[i] = previous[i-1] + previous[i-2];
+        }
+        return previous[n];
     }
 };
